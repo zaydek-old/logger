62be7173a2ea929bcfd88005a7c83d7c0f6d639d
Zaydek MG <zaydek@icloud.com>
2018-02-15 06:55:21 +0900

    docs

--- logger.go
+++ logger.go
@@ -6,22 +6,26 @@ import (
 	"sync"
 )
 
+// Logger is mutex-protected.
 type Logger struct {
 	m sync.Mutex
 	w io.Writer
 	f func() string
 }
 
+// New returns a new Logger.
 func New(w io.Writer, f func() string) *Logger {
 	return &Logger{f: f, w: w}
 }
 
+// Printf prefixes the Logger's output with the string returned from the Logger's f func.
 func (l *Logger) Printf(format string, a ...interface{}) {
 	l.m.Lock()
 	fmt.Fprintf(l.w, format, prepend(l.f(), a)...)
 	l.m.Unlock()
 }
 
+// Println prefixes the Logger's output with the string returned from the Loggers' f func.
 func (l *Logger) Println(a ...interface{}) {
 	l.m.Lock()
 	fmt.Fprintln(l.w, prepend(l.f(), a)...)
